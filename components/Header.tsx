5a0138574cc5d8e317034dc1369dcdc661d2d0a3
Taha Hammouz <t.r.hamooz@gmail.com>
2023-10-11 13:30:09 +0300

    images container component

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,6 @@ import Link from "next/link";
 const Header = () => {
   return (
     <div className="flex justify-between p-5 sticky bg-white z-50 shadow-md">
-      {/* left div */}
       <div className="flex space-x-2 items-center">
         <Image
           src="https://links.papareact.com/4t3"
@@ -16,16 +15,14 @@ const Header = () => {
 
         <div>
           <h1 className="font-bold">
-            The PAPAFAM <span className="text-violet-500">AI</span> Image
-            Generator
+            <span className="text-violet-500">AI</span> Image Generator
           </h1>
           <h2 className="text-xs">
-            Powered by DALL.E 2, ChatGPT & Microsoft Azure!
+            Powered by DALL.E 2, ChatGPT & Microsoft Azure, created by Taha
+            Hammouz!
           </h2>
         </div>
       </div>
-
-      {/* right div */}
 
       <div className="flex text-xs md:text-base divide-x items-center">
         <Link
