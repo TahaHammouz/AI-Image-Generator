676d2e7cdce91eb8de5471d1c10d0d7052bcb56f
Taha Hammouz <t.r.hamooz@gmail.com>
2023-10-11 11:51:35 +0300

    suggestion api

--- app/api/suggestion/route.ts
+++ app/api/suggestion/route.ts
@@ -1,9 +1,12 @@
 export async function GET(request: Request) {
-  const response = await fetch("", {
-    cache: "no-store",
-  });
+  const response = await fetch(
+    "http://localhost:7071/api/getChatGPTSuggestion",
+    {
+      cache: "no-store",
+    }
+  );
 
   const textData = await response.text();
 
-  return new Response(JSON.stringify(textData.trim), { status: 200 });
+  return new Response(JSON.stringify(textData.trim()), { status: 200 });
 }
