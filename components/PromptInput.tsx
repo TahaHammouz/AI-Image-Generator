397282c7c123413538b9ca06744d64e20ef8b190
Taha Hammouz <t.r.hamooz@gmail.com>
2023-10-11 13:30:27 +0300

    mutate rendered images

--- components/PromptInput.tsx
+++ components/PromptInput.tsx
@@ -1,15 +1,80 @@
 "use client";
 
-import { useState } from "react";
+import useSWR from "swr";
+import { FormEvent, useState } from "react";
+import fetchSuggestionFromChatGPT from "../lib/fetchSuggestionFromChatGPT";
+import fetchImages from "../lib/fetchImages";
+import toast from "react-hot-toast";
 
 function PromptInput() {
   const [input, setInput] = useState("");
 
+  const {
+    data: suggestion,
+    isLoading,
+    mutate,
+    isValidating,
+  } = useSWR("/api/suggestion", fetchSuggestionFromChatGPT, {
+    revalidateOnFocus: false,
+  });
+  const { mutate: updateImages } = useSWR("images", fetchImages, {
+    revalidateOnFocus: false,
+  });
+
+  const loading = isLoading || isValidating;
+
+  const submitPrompt = async (useSuggestion?: boolean) => {
+    const inputPrompt = input;
+
+    setInput("");
+    console.log(inputPrompt);
+
+    const notificationPrompt = inputPrompt || suggestion;
+    const notificationPromptShort = notificationPrompt.slice(0, 20);
+
+    const notification = toast.loading(
+      `DALL·E is creating: ${notificationPromptShort}...`
+    );
+
+    const p = useSuggestion ? suggestion : inputPrompt;
+
+    const res = await fetch("/api/generateImage", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt: p }),
+    });
+
+    const data = await res.json();
+    if (data.error) {
+      toast.error(data.error);
+    } else {
+      toast.success(`Your AI Art has been Generated!`, {
+        id: notification,
+      });
+    }
+
+    updateImages();
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    await submitPrompt();
+  };
+
   return (
     <div className="m-10">
-      <form className="flex flex-col lg:flex-row shadow-md shadow-slate-400/10 border rounded-md lg:divide-x">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col lg:flex-row shadow-md shadow-slate-400/10 border rounded-md lg:divide-x">
         <textarea
-          placeholder={"Enter a prompt..."}
+          placeholder={
+            (loading && "ChatGPT is thinking") ||
+            suggestion ||
+            "Enter a prompt..."
+          }
           value={input}
           onChange={(e) => setInput(e.target.value)}
           className="flex-1 p-4 outline-none rounded-md"
@@ -26,15 +91,26 @@ function PromptInput() {
         </button>
         <button
           className={`p-4 bg-violet-400 text-white transition-colors duration-200 font-bold disabled:text-gray-300 disabled:cursor-not-allowed disabled:bg-gray-400`}
-          type="button">
+          type="button"
+          onClick={() => submitPrompt(true)}>
           Use Suggestion
         </button>
         <button
           className={`p-4 bg-white text-violet-500 border-none transition-colors duration-200 rounded-b-md md:rounded-r-md md:rounded-bl-none font-bold`}
-          type="button">
+          type="button"
+          onClick={mutate}>
           New Suggestion
         </button>
       </form>
+
+      {input && (
+        <p className="italic pt-2 pl-2 font-light">
+          Suggestion:{" "}
+          <span className="text-violet-500">
+            {loading ? "ChatGPT is thinking" : suggestion}
+          </span>
+        </p>
+      )}
     </div>
   );
 }
