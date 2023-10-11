6399a755a0cd09300bfedca1bc29dcb07bd090e6
Taha Hammouz <t.r.hamooz@gmail.com>
2023-10-11 13:28:19 +0300

    add images url

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   images: {
     domains: [
-      "sonnytestapp38d554.blob.core.windows.net",
+      "aiimagesgenerator.blob.core.windows.net",
       "links.papareact.com",
     ],
   },
