fc649e34c7d99b53f9334a829c097669755f7557
Taha Hammouz <t.r.hamooz@gmail.com>
2023-10-11 13:31:42 +0300

    fetch data from api

---
+++ app/api/getImages/route.ts
@@ -0,0 +1,14 @@
+export async function GET(request: Request) {
+  const response = await fetch("http://localhost:7071/api/getImages", {
+    cache: "no-store",
+  });
+
+  const blob = await response.blob();
+  const textData = await blob.text();
+
+  const data = JSON.parse(textData);
+
+  return new Response(JSON.stringify(data), {
+    status: 200,
+  });
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-
+import Images from "../components/Images";
 
 async function HomePage() {
   return (
     <div className="mx-0 md:10">
+      <Images />
     </div>
   );
 }
